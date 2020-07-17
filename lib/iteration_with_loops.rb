b12d61f75746d146e5e9f4d07bf1c0be2131bad5
Shelley Brook <sbrook13@gmail.com>
2020-07-17 21:17:46 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,5 +1,10 @@
 def join_nested_strings(src)
   row_index = 0
-  
+  sorted_array = ""
+  while row_index < src.length do
+    element_index = 0
+    while element_index < src[row_index].length do
+      if src[row_index][element_index].is_a?(String)
+        
 
 end
